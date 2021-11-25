71d6003c248b62953a8d75dd16d326a13c255e0e
ZiTe <honmonoh@gmail.com>
2021-11-25 16:49:06 +0800

    Add connection items

--- CoppeliaSimCSharpAPI.TestForm/Form1.Designer.cs
+++ CoppeliaSimCSharpAPI.TestForm/Form1.Designer.cs
@@ -28,13 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.numericUpDownPort = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(93, 61);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(159, 73);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Location = new System.Drawing.Point(93, 163);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(159, 73);
+            this.buttonDisconnect.TabIndex = 0;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(348, 138);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(100, 31);
+            this.textBoxAddress.TabIndex = 1;
+            this.textBoxAddress.Text = "127.0.0.1";
+            // 
+            // numericUpDownPort
+            // 
+            this.numericUpDownPort.Location = new System.Drawing.Point(477, 139);
+            this.numericUpDownPort.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.numericUpDownPort.Name = "numericUpDownPort";
+            this.numericUpDownPort.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDownPort.TabIndex = 2;
+            this.numericUpDownPort.Value = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1270, 626);
+            this.Controls.Add(this.numericUpDownPort);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "Form1";
+            this.Text = "CoppeliaSim C# API Test Form";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.NumericUpDown numericUpDownPort;
     }
 }
 
--- CoppeliaSimCSharpAPI.TestForm/Form1.cs
+++ CoppeliaSimCSharpAPI.TestForm/Form1.cs
@@ -12,9 +12,21 @@ namespace CoppeliaSimCSharpAPI.TestForm
 {
     public partial class Form1 : Form
     {
+        private int _id;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            _id = Api.Connect(textBoxAddress.Text, (int)numericUpDownPort.Value);
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            Api.Disconnect(_id);
         }
     }
 }
