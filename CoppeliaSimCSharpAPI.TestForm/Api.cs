f05e56f9d800b9809b3670727cf88a8479a98b20
ZiTe <honmonoh@gmail.com>
2021-11-25 13:09:10 +0800

    Add Api.cs for import DLL

---
+++ CoppeliaSimCSharpAPI.TestForm/Api.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CoppeliaSimCSharpAPI.TestForm
+{
+    internal class Api
+    {
+        [DllImport("CoppeliaSimCSharpAPI.dll")]
+        public static extern int Connect(string address, int port);
+
+        [DllImport("CoppeliaSimCSharpAPI.dll")]
+        public static extern int Disconnect(int id);
+
+        [DllImport("CoppeliaSimCSharpAPI.dll")]
+        public static extern int GetJointPosition(int id, int[] jointHandle, float[] position);
+
+        [DllImport("CoppeliaSimCSharpAPI.dll")]
+        public static extern int GetObjectHandle(int id, string objectName);
+
+        [DllImport("CoppeliaSimCSharpAPI.dll")]
+        public static extern int MoveJoint(int id, float[] position, int[] jointHandle, bool inTorqueForceMode);
+    }
+}
