ca449d83ce1851ec6e76636c496e66a9fa36af12
ZiTe <honmonoh@gmail.com>
2021-11-25 16:55:42 +0800

    Add get joint handle

--- CoppeliaSimCSharpAPI.TestForm/Form1.cs
+++ CoppeliaSimCSharpAPI.TestForm/Form1.cs
@@ -13,6 +13,7 @@ namespace CoppeliaSimCSharpAPI.TestForm
     public partial class Form1 : Form
     {
         private int _id;
+        private int[] _jointHandle = new int[6];
 
         public Form1()
         {
@@ -22,6 +23,10 @@ namespace CoppeliaSimCSharpAPI.TestForm
         private void buttonConnect_Click(object sender, EventArgs e)
         {
             _id = Api.Connect(textBoxAddress.Text, (int)numericUpDownPort.Value);
+            for (int i = 0; i < _jointHandle.Length; i++)
+            {
+                _jointHandle[i] = Api.GetObjectHandle(_id, $"UR5_joint{i + 1}");
+            }
         }
 
         private void buttonDisconnect_Click(object sender, EventArgs e)
